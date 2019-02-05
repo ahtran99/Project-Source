e23989f8094488b67d8dc7d9a78d0f5e02f11efc
ahtran99 <44480650+ahtran99@users.noreply.github.com>
2019-02-05 11:02:01 -0500

    Add files via upload

---
+++ GlobalScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalScore : MonoBehaviour
+{
+
+    public static int CurrentScore;
+    public int InternalScore;
+    public GameObject ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalScore = CurrentScore;
+        ScoreText.GetComponent<Text>().text = "" + InternalScore;
+    }
+}
